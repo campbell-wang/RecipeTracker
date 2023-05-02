6544cf3f8882e93b1530e56ce192714eedae2b5c
campbell-w <campbell.w@scentroid.com>
2023-05-02 22:20:51 +0800

    Initial Commit

---
+++ recipetracker/recipetrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipetrackerApp.swift
+//  recipetracker
+//
+//  Created by Campbell Wang on 2023-05-02.
+//
+
+import SwiftUI
+
+@main
+struct recipetrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
